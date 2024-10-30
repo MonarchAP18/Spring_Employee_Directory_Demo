c6c1680b21b0d96420474605bfa0645c2435e6b5
Akshay Uday Padamwar <akshaypadamwar12@gmail.com>
2024-10-30 20:36:55 +0530

    Initial Commit

---
+++ src/test/java/com/SpringBootCourse/EmployeeDirectory/EmployeeDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootCourse.EmployeeDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
